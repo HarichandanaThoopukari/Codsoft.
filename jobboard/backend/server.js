b9fb4145c427d628c908be59864182d3ca0bbb86
HarichandanaThoopukari <thoopukariharichandana@gmail.com>
2026-06-13 17:44:35 +0530

    Update server.js

--- jobboard/backend/server.js
+++ jobboard/backend/server.js
@@ -22,6 +22,7 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/jobs', require('./routes/jobs'));
 app.use('/api/applications', require('./routes/applications'));
 app.use('/api/users', require('./routes/users'));
+app.use('/api/admin', require('./routes/admin'));
 
 app.get('/', (req, res) => res.json({ message: 'JobBoard API Running' }));
 
